64e60f324bd5239a0d43812df18abb28b37f828f
Jorge Zupirolli <jorgebertolo@gmail.com>
2021-05-21 13:43:14 -0400

    change: plugin.js to plugins.js on path: config

--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,11 @@
-module.exports = ({ env }) => {
-  if (env('NODE_ENV' === 'production')) {
-    return {
-      upload: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {}
-        }
-      },
-    }
-  }
-
-  return {
-
-  }
-};
+module.exports = ({ env }) => ({
+  upload: {
+    provider: env("MEDIA_PROVIDER", "cloudinary"),
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+      upload_preset: env("CLOUDINARY_PRESET", "local"),
+    },
+  },
+});
